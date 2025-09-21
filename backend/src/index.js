32c7bd005f6b7e1394e91b7cb14767b0a9f946dc
Estácio Ramalho <estacioramalhodev@gmail.com>
2025-09-20 22:04:09 -0300

    fix: improve route handling for serving index.html in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,8 +30,8 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/:path*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
